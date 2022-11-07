89ddb8ab1a052641fadcfa0ef171755129624115
fukinlab <33475349+gom04@users.noreply.github.com>
2022-11-07 18:24:47 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/* @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+ 
+  ],
+  theme: {
+    extend: { 
+      spacing: {
+        '1': '8px',
+        '2': '12px',
+        '3': '16px',
+        '4': '24px',
+        '5': '32px',
+        '6': '48px',
+      }
+
+    },
+  },
+  plugins: [],
+}
